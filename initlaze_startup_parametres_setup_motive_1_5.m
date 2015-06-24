46bd26fecce967d4144802488eb5547f936b883e
or hirshfeld <orhirshfeld@gmail.com>
2015-06-24 18:57:12 +0300

    new ver
    new ver

---
+++ initlaze_startup_parametres_setup_motive_1_5.m
@@ -0,0 +1,11 @@
+clear all;
+addpath('D:\OneDrive\Motive_1_5_Connect'); % location for block icons
+
+% Motiove project file contain camera calibration and trackable setup
+project_file='D:\OneDrive\Motive_1_5_Connect\Motive_1_5_Project.ttp';
+
+% location contains .dll (dynamic libary) and .lib (static libary) files
+inc_location='D:\OneDrive\Motive_1_5_Connect\inc';
+
+% location containd fixed .h files (header)
+lib_location='D:\OneDrive\Motive_1_5_Connect\lib';
